d92a763404855e11dc5ec2885cffbee150de2425
Gustavo Leme <gleme@nevada.unr.edu>
2020-03-02 22:37:17 -0300

    fixes coverage config

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: '../coverage',
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -35,10 +35,10 @@ module.exports = {
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
+  //   'json',
+  //   'text',
+  //   'lcov',
+  //   'clover'
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
